621a467db8c3bc6f1c08deaa7456825e7f4e6c7c
tigas2909 <t47801375@gmail.com>
2026-04-21 13:54:10 -0300

    Delete 4_CRUD_parte1/4_CRUD directory

--- 4_CRUD_parte1/4_CRUD/vendedorTable.sql
+++
@@ -1,7 +0,0 @@
-create table IF NOT EXISTS vendedor(
-    codigo int not null primary key,
-    nome varchar(50) not null,
-    email varchar(80) not null,
-    vendas int not null,
-    status_vendedor enum('ativo', 'inativo') not null DEFAULT 'ativo'
-);
